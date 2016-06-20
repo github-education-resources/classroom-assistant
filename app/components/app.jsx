4f3c28749f8c334c3d215d5da3ac1bc46454ef59
Nick Tikhonov <nicktikhonov@github.com>
2016-06-20 11:29:22 -0700

    fix style errors and add a few photonkit components

--- app/components/app.jsx
+++ app/components/app.jsx
@@ -1,13 +1,15 @@
 "use strict"
 
-const React = require('react')
-const Window = require('react-photonkit').Window
+const React = require("react")
+const {Window, Toolbar, Content} = require("react-photonkit")
 
 var App = React.createClass({
   render: () => {
     return (
       <Window>
-        <h1>Hello World!</h1>
+        <Toolbar title='Classroom Desktop'/>
+          <Content />
+        <Toolbar psType='footer' title='Classroom Desktop'/>
       </Window>
     )
   }
