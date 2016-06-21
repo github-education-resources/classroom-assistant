cc2d1f1eb104499c9c27fbc166176665679cd90f
Nick Tikhonov <nicktikhonov@github.com>
2016-06-21 14:55:37 -0700

    barebones react redux app

--- app/index.jsx
+++ app/index.jsx
@@ -1,43 +1,100 @@
 import * as React from "react"
 import * as ReactDOM from "react-dom"
 import {Window, Toolbar, Content, Pane, ListGroup, ListItem} from "react-photonkit"
-import {createStore} from "redux"
+import {createStore, compose} from "redux"
+import DevTools from "./DevTools"
 
-const reducer = (state, action) => {
+const initialState = [
+  {
+    id: 1,
+    text: "Item 1",
+    active: false
+  },
+  {
+    id: 2,
+    text: "Item 2",
+    active: true
+  }
+]
+
+const itemReducer = (state, action) => {
   switch (action.type) {
   case "SELECT_ITEM":
-    return state
+    if (action.id === state.id) {
+      return Object.assign({}, state, {active: !state.active})
+    } else {
+      return state
+    }
   default:
     return state
   }
 }
 
+const reducer = (state, action) => {
+  if (typeof state === "undefined") {
+    return initialState
+  }
+
+  switch (action.type) {
+  case "SELECT_ITEM":
+    return state.map((item) => {
+      return itemReducer(item, action)
+    })
+  default:
+    return state
+  }
+}
+
+const store = createStore(reducer, compose(DevTools.instrument()))
+
 const render = () => {
   ReactDOM.render(
-    <App />
+    <App
+      todos={store.getState()}
+      handleItemClick={(id) => {
+        store.dispatch({
+          type: "SELECT_ITEM",
+          id: id
+        })
+      }}/>
   , document.getElementById("app"))
 }
 
-const store = createStore(reducer)
+const Item = ({
+  text,
+  active,
+  handleClick
+}) => {
+  return <ListItem
+      image="https://avatars3.githubusercontent.com/u/1744446?v=3&s=400"
+      title={text}
+      subtitle="subtitle"
+      onClick={handleClick}
+      active={active} />
+}
 
-const App = () => {
+const App = ({
+  todos,
+  handleItemClick
+}) => {
   return (
     <Window>
     <Toolbar title="header" />
     <Content>
       <Pane>
         <ListGroup>
-          <ListItem
-            image="https://avatars3.githubusercontent.com/u/1744446?v=3&s=400"
-            title="list item"
-            subtitle="subtitle"
-            active />
-          <ListItem
-            image="https://avatars3.githubusercontent.com/u/1744446?v=3&s=400"
-            title="list item"
-            subtitle="subtitle" />
+          {todos.map((todo) => {
+            return (
+              <Item
+                text={todo.text}
+                active={todo.active}
+                handleClick={() => { handleItemClick(todo.id) }}
+                />
+            )
+          })}
         </ListGroup>
       </Pane>
+      <DevTools store={store}/>
     </Content>
     <Toolbar psType="footer" title="footer" />
   </Window>
