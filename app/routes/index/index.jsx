e37c82a4f1a8753b1389928156e4ece64d0b5972
Nick Tikhonov <nicktikhonov@github.com>
2016-07-06 18:26:16 -0700

    Add index view

--- app/routes/index/index.jsx
+++ app/routes/index/index.jsx
@@ -1,10 +1,26 @@
 import React from "react"
 import { Link } from "react-router"
 
+const containerStyles = {
+  paddingTop: "50px"
+}
+
 const LaunchView = () => (
-  <div>
-    <p>Launch View</p>
-    <Link to="select"> Select Submissions </Link>
+  <div style={containerStyles}>
+    <div className="row">
+      <div className="col-sm-6 col-sm-offset-3">
+        <h1 className="text-center">GitHub Classroom Desktop</h1>
+        <p className="lead text-center">
+          GitHub Classroom Desktop lets you archive assignments submitted on GitHub
+          Classroom with a click of a button.
+        </p>
+          <div className="text-center">
+            <Link to="select">
+              <button className="btn btn-primary btn-lg">Get started on GitHub Classroom</button>
+            </Link>
+          </div>
+      </div>
+    </div>
   </div>
 )
 
