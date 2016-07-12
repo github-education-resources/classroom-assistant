e277f94c90f516a69332bb746d62c6192e4fffc5
Nick Tikhonov <nicktikhonov@github.com>
2016-07-12 11:07:01 -0700

    Add babel strict transform, move devtools outside routes

--- app/index.jsx
+++ app/index.jsx
@@ -4,6 +4,7 @@ import { hashHistory } from "react-router"
 import { syncHistoryWithStore } from "react-router-redux"
 import { createStore, compose } from "redux"
 import { Provider } from "react-redux"
+import isDev from "electron-is-dev"
 
 import DevTools from "./devtools"
 import reducer from "./modules/reducers"
@@ -12,10 +13,18 @@ import Routes from "./routes"
 const store = createStore(reducer, compose(DevTools.instrument()))
 const history = syncHistoryWithStore(hashHistory, store)
 
+let devToolsInstance
+if (isDev) {
+  devToolsInstance = <DevTools />
+}
+
 const render = () => {
   ReactDOM.render(
     <Provider store={store}>
-      <Routes history={history} />
+      <div>
+        <Routes history={history} />
+        {devToolsInstance}
+      </div>
     </Provider>
   , document.getElementById("app"))
 }
--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,33 +1,23 @@
 import React, { PropTypes } from "react"
-import isDev from "electron-is-dev"
 import { Router, Route } from "react-router"
 
-import DevTools from "../DevTools"
 import IndexPage from "./index/index"
 import SelectPage from "./select/index"
 import ConfirmPage from "./confirm/index"
 import ArchivePage from "./archive/index"
 import SettingsPage from "./settings/index"
 
-let devToolsInstance
-if (isDev) {
-  devToolsInstance = <DevTools />
-}
-
 const Routes = ({
   history
 }) => {
   return (
-    <div>
-      <Router history={history}>
-        <Route name="index" path="/" component={IndexPage} />
-        <Route name="select" path="/select" component={SelectPage} />
-        <Route name="confirm" path="/confirm" component={ConfirmPage} />
-        <Route name="archive" path="/archive" component={ArchivePage} />
-        <Route name="settings" path="/settings" component={SettingsPage} />
-      </Router>
-      {devToolsInstance}
-    </div>
+    <Router history={history}>
+      <Route name="index" path="/" component={IndexPage} />
+      <Route name="select" path="/select" component={SelectPage} />
+      <Route name="confirm" path="/confirm" component={ConfirmPage} />
+      <Route name="archive" path="/archive" component={ArchivePage} />
+      <Route name="settings" path="/settings" component={SettingsPage} />
+    </Router>
   )
 }
 
