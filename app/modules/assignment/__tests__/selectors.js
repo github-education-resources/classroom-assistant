a31f2d9cc3a64c33fe247a48c15fe9e7f279168b
Nick Tikhonov <nicktikhonov@github.com>
2016-07-15 11:52:44 -0700

    Add tests for assignment selectors

---
+++ app/modules/assignment/__tests__/selectors.js
@@ -0,0 +1,48 @@
+jest.unmock("../selectors.js")
+
+import { all, typeLabel } from "../selectors.js"
+
+describe("assignmnet selectors", () => {
+  let testIndividualAssignment = {
+    name: "Assignment 1: Introduction to Programming",
+    type: "individual"
+  }
+
+  let testGroupAssignment = {
+    name: "Assignment 1: Introduction to Programming",
+    type: "group"
+  }
+
+  let testInvalidAssignment = {
+    name: "Assignment 1: Introduction to Programming",
+    type: "somethingelse"
+  }
+
+  describe("all", () => {
+    it("returns whole assignment object given state", () => {
+      expect(all({
+        assignment: testIndividualAssignment
+      })).toBe(testIndividualAssignment)
+    })
+  })
+
+  describe("typeLabel", () => {
+    it("returns correct label for individual assignment", () => {
+      expect(typeLabel({
+        assignment: testIndividualAssignment
+      })).toBe("Individual Assignment")
+    })
+
+    it("returns correct label for group assignment", () => {
+      expect(typeLabel({
+        assignment: testGroupAssignment
+      })).toBe("Group Assignment")
+    })
+
+    it("returns default label for invalid assignment types", () => {
+      expect(typeLabel({
+        assignment: testInvalidAssignment
+      })).toBe("Unknown Assignment Type")
+    })
+  })
+})
