ed6a9677bd9209d0b1a07627bff9dc6da2628df2
Nick Tikhonov <nicktikhonov@github.com>
2016-07-18 14:12:33 -0700

    Refactor module and tests

--- app/lib/__tests__/cloneutils-test.js
+++ app/lib/__tests__/cloneutils-test.js
@@ -7,57 +7,67 @@ import sinon from "sinon"
 const fs = require("fs")
 
 const TEST_REPO = "https://github.com/education/classroom-desktop"
+const TEST_FAKE_REPO = "https://github.com/education/a-repo-that-will-never-exist"
 const DESTINATION = "/tmp/" + Math.random().toString(36).substring(7)
 
 describe("Clone Utilities", () => {
   describe("clone", () => {
-    it("clones the repository", (done) => {
+    const removeTestDir = (done) => {
       rmdir(DESTINATION, () => {
-        clone(
-          TEST_REPO,
-          DESTINATION,
-          (percentage) => {}
-        ).then(() => {
-          const stats = fs.lstatSync(DESTINATION)
-          expect(stats.isDirectory()).toBe(true)
-          rmdir(DESTINATION, () => {
-            done()
-          })
-        })
+        done()
+      })
+    }
+
+    beforeEach(removeTestDir)
+    afterEach(removeTestDir)
+
+    it("clones the repository to the correct destination", (done) => {
+      clone(
+        TEST_REPO,
+        DESTINATION,
+        (percentage) => {}
+      ).then(() => {
+        const stats = fs.lstatSync(DESTINATION)
+        expect(stats.isDirectory()).toBe(true)
+        done()
       })
     })
 
     it("notifies when the repo is 0% downloaded", (done) => {
-      rmdir(DESTINATION, () => {
-        var statusCallback = sinon.spy()
+      const statusCallback = sinon.spy()
 
-        clone(
-          TEST_REPO,
-          DESTINATION,
-          statusCallback
-        ).then(() => {
-          expect(statusCallback.calledWith(0)).toBe(true)
-          rmdir(DESTINATION, () => {
-            done()
-          })
-        })
+      clone(
+        TEST_REPO,
+        DESTINATION,
+        statusCallback
+      ).then(() => {
+        expect(statusCallback.calledWith(0)).toBe(true)
+        done()
       })
     })
 
     it("notifies when the repo is 100% downloaded", (done) => {
-      rmdir(DESTINATION, () => {
-        var statusCallback = sinon.spy()
+      const statusCallback = sinon.spy()
 
-        clone(
-          TEST_REPO,
-          DESTINATION,
-          statusCallback
-        ).then(() => {
-          expect(statusCallback.calledWith(100)).toBe(true)
-          rmdir(DESTINATION, () => {
-            done()
-          })
-        })
+      clone(
+        TEST_REPO,
+        DESTINATION,
+        statusCallback
+      ).then(() => {
+        expect(statusCallback.calledWith(100)).toBe(true)
+        done()
+      })
+    })
+
+    it("throws an error when cloning a repo that doesn't exist", (done) => {
+      clone(
+        TEST_FAKE_REPO,
+        DESTINATION,
+        () => {}
+      ).then(() => {
+        fail("Clone should not succeed")
+      }).catch(() => {
+        done()
       })
     })
   })
--- app/lib/cloneutils.js
+++ app/lib/cloneutils.js
@@ -1,36 +1,64 @@
 const NodeGit = require("nodegit")
 
+// Internal: Helper function that constructs the options object used by NodeGit.
+const buildOptions = (progressCallback) => {
+  const callbacks = new NodeGit.RemoteCallbacks()
+  callbacks.transferProgress = progressCallback
+
+  const fetchOptions = new NodeGit.FetchOptions()
+  fetchOptions.callbacks = callbacks
+
+  const options = new NodeGit.CloneOptions()
+  options.fetchOpts = fetchOptions
+
+  return options
+}
+
+// Public: Clones a public git repository to the specified destination directory,
+// and notifies the caller of clone progress via callback.
+//
+// repoURL          - String containing the URL of the public Git repository. It
+//                    should be possible to clone the repository with `git clone`
+// destination      - String containing Absolute path where the repository files will
+//                    be cloned.
+// progressCallback - Callback Function used for notifying the caller of clone
+//                    progress. The function is called with the percentage of object
+//                    fetched.
+//
+// Examples
+//
+//    clone(
+//      "https://github.com/education/classroom-desktop",
+//      "/tmp/desktop-classroom",
+//      (percentage) => {
+//        console.log("Percentage complete: " + percentage + "%")
+//      }
+//    ).then(() => {
+//      console.log("Finished cloning")
+//    })
+//
+// Returns a Promise
 export const clone = (repoURL, destination, progressCallback) => {
   return new Promise((resolve, reject) => {
-    const options = new NodeGit.CloneOptions()
+    let progressOnCompletion = false
 
-    const fetchOptions = new NodeGit.FetchOptions()
-    options.fetchOpts = fetchOptions
-
-    const callbacks = new NodeGit.RemoteCallbacks()
-    fetchOptions.callbacks = callbacks
-
-    let progressNotifiedOnCompletion = false
-
-    callbacks.transferProgress = (something) => {
-      const percentage = 100 * something.receivedObjects() / something.totalObjects()
-
-      if (percentage === 100) {
-        progressNotifiedOnCompletion = true
-      }
-
+    const options = buildOptions((progressInfo) => {
+      const percentage = 100 * progressInfo.receivedObjects() / progressInfo.totalObjects()
+      if (percentage === 100) progressOnCompletion = true
       progressCallback(percentage)
-    }
+    })
 
     NodeGit.Clone(
       repoURL,
       destination,
       options
     ).then(() => {
-      if (!progressNotifiedOnCompletion) {
+      if (!progressOnCompletion) {
         progressCallback(100)
       }
       resolve()
+    }).catch((err) => {
+      reject(err)
     })
   })
 }
