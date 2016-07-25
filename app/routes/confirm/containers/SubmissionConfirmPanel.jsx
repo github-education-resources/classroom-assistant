3019f9bd99e0841ff7e5c067d6fbf2a77191831a
Nick Tikhonov <nicktikhonov@github.com>
2016-07-25 07:47:09 -0700

    Remove edit button from submission confirm panel

--- app/routes/confirm/containers/SubmissionConfirmPanel.jsx
+++ app/routes/confirm/containers/SubmissionConfirmPanel.jsx
@@ -1,11 +1,11 @@
-import EditItemPanel from "../../shared/components/EditItemPanel"
+import ItemPanel from "../../shared/components/ItemPanel"
 import { connect } from "react-redux"
 import { num, numSelected } from "../../../modules/submissions/selectors"
 
 const mapStateToProps = (state) => ({
   title: `${numSelected(state)}/${num(state)}`,
-  iconPath: "http://placehold.it/48x48",
+  imagePath: "http://placehold.it/48x48",
   subtitle: "Submissions Selected"
 })
 
-export default connect(mapStateToProps, null)(EditItemPanel)
+export default connect(mapStateToProps, null)(ItemPanel)
