1e505d28fb7dc76f7cdee4c19e9379742dbcf325
Nick Tikhonov <nicktikhonov@github.com>
2016-07-27 09:48:33 -0700

    Add settings to the state

--- app/modules/reducers.js
+++ app/modules/reducers.js
@@ -3,9 +3,11 @@ import { routerReducer } from "react-router-redux"
 
 import submissions from "./submissions/reducers/submissions"
 import assignment from "./assignment/reducers/assignment"
+import settings from "./settings/reducers/settings"
 
 export default combineReducers({
   submissions,
   assignment,
+  settings,
   routing: routerReducer
 })
---
+++ app/modules/settings/reducers/settings.js
@@ -0,0 +1,9 @@
+const initialState = {
+  cloneDestination: "/tmp"
+}
+
+const settings = (state, action) => {
+  return initialState
+}
+
+export default settings
