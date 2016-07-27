8faad08eed95768c83abc0df5ddc2dfe2ee55dc5
Nick Tikhonov <nicktikhonov@github.com>
2016-07-27 10:09:55 -0700

    Add reducer for changing destination

--- app/modules/settings/reducers/settings.js
+++ app/modules/settings/reducers/settings.js
@@ -1,9 +1,16 @@
+import { SETTINGS_CHANGE_CLONE_DESTINATION } from "../constants"
+
 const initialState = {
   cloneDestination: "/tmp"
 }
 
-const settings = (state, action) => {
-  return initialState
+const settings = (state = initialState, action) => {
+  switch (action.type) {
+  case SETTINGS_CHANGE_CLONE_DESTINATION:
+    return Object.assign({}, state, {cloneDestination: action.destination})
+  default:
+    return state
+  }
 }
 
 export default settings
