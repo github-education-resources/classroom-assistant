b697e41da0d79e2e2a95ccf371fb0f6eba5215c8
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2018-07-12 16:09:09 -0700

    remove history and add main process changes in single commit

---
+++ app/userAuthentication.js
@@ -0,0 +1,59 @@
+import fetch from "electron-fetch/lib"
+import appInfo from "./app-info.json"
+import keytar from "keytar"
+import {BrowserWindow, session} from "electron"
+
+let mainWindow, authWindow
+
+const clientId = appInfo["client_id"]
+const clientSecret = appInfo["client_secret"]
+const requiredScopes = ["repo"].join("%20")
+
+export function authorizeUser (mainWindowRef) {
+  mainWindow = mainWindowRef
+  openAuthWindow()
+}
+
+export function fetchAccessToken (code) {
+  authWindow.destroy()
+  var data = {
+    client_id: clientId,
+    client_secret: clientSecret,
+    code: code,
+  }
+  fetch("https://github.com/login/oauth/access_token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then(response => response.json())
+    .then(data => {
+      keytar.setPassword("Classroom-Desktop", "token", data.access_token)
+      mainWindow.webContents.send("receivedAuthorization")
+    })
+    .catch((error) => console.log(error))
+}
+
+function openAuthWindow () {
+  authWindow = new BrowserWindow({
+    parent: mainWindow,
+    modal: true,
+    height: 600,
+    width: 400,
+    show: false,
+    webPreferences: {
+      session: session.defaultSession,
+    }
+  })
+  console.log(`https://github.com/login/oauth/authorize?client_id=${clientId}&scope=${requiredScopes}`)
+  authWindow.loadURL(`https://github.com/login/oauth/authorize?client_id=${clientId}&scope=${requiredScopes}`)
+
+  authWindow.once("ready-to-show", () => {
+    if (authWindow) {
+      authWindow.show()
+    }
+  })
+}
