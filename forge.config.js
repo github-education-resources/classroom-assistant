a003525d1616e1ded46bfc39b2f220b9331e89af
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2018-08-16 16:22:20 -0700

    revert any cpu because of npm install on appveyor

--- forge.config.js
+++ forge.config.js
@@ -25,7 +25,7 @@ module.exports = {
     ]
   },
   electronPackagerConfig: {
-    packageManager: "npm",
+    packageManager: "yarn",
     osxSign: {
       identity: getOSXSigningIdentity()
     },
