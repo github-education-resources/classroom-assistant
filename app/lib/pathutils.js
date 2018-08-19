2e91560d3b8838727aa383c30c09cd013e6cc60e
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2018-08-18 18:53:35 -0700

    add mkdirp since dugite doesn't automatically create folder

--- app/lib/pathutils.js
+++ app/lib/pathutils.js
@@ -1,7 +1,14 @@
 import path from "path"
 import dateFormat from "dateformat"
+import mkdirp from "mkdirp"
 
 export const getClonePath = (basePath, assignmentName, studentUsername) => {
   const time = dateFormat(new Date(), "mm-dd-yyyy-hh-MM-ss")
-  return path.join(basePath, `${assignmentName}-${time}`, studentUsername)
+  const submissionPath = path.join(basePath, `${assignmentName}-${time}`, studentUsername)
+  try {
+    mkdirp(submissionPath)
+    return submissionPath
+  } catch (error) {
+    return null
+  }
 }
