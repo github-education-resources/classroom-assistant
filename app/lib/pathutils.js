1cd22dae8d2b24179efa681bca09cacd05941292
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2018-08-19 13:28:57 -0700

    add fs-extra to asynchronously create folder for cloning

--- app/lib/pathutils.js
+++ app/lib/pathutils.js
@@ -1,12 +1,12 @@
 import path from "path"
 import dateFormat from "dateformat"
-import mkdirp from "mkdirp"
+import fs from "fs-extra"
 
-export const getClonePath = (basePath, assignmentName, studentUsername) => {
+export const getClonePath = async (basePath, assignmentName, studentUsername) => {
   const time = dateFormat(new Date(), "mm-dd-yyyy-hh-MM-ss")
   const submissionPath = path.join(basePath, `${assignmentName}-${time}`, studentUsername)
   try {
-    mkdirp(submissionPath)
+    await fs.ensureDir(submissionPath)
     return submissionPath
   } catch (error) {
     return null
