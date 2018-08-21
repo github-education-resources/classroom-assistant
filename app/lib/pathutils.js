034f1a3f5bac3589094f776638e9ab58de2ce3ad
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2018-08-21 16:29:44 -0700

    add sanitize to filenames for paths

--- app/lib/pathutils.js
+++ app/lib/pathutils.js
@@ -1,14 +1,15 @@
 import path from "path"
 import dateFormat from "dateformat"
 import fs from "fs-extra"
+import sanitize from "sanitize-filename"
 
 export const getAssignmentFolder = (basePath, assignmentName) => {
   const time = dateFormat(new Date(), "mm-dd-yyyy-hh-MM-ss")
-  return path.join(basePath, `${assignmentName}-${time}`)
+  return path.join(basePath, sanitize(`${assignmentName}-${time}`))
 }
 
 export const getClonePath = async (basePath, studentUsername) => {
-  const submissionPath = path.join(basePath, studentUsername)
+  const submissionPath = path.join(basePath, sanitize(studentUsername))
   try {
     await fs.ensureDir(submissionPath)
     return submissionPath
