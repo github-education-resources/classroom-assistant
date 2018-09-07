0b726e4137a5dda060500e27f8957987df52e75c
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2018-09-07 11:50:26 -0500

    mac os code sign support testing

--- forge.config.js
+++ forge.config.js
@@ -1,14 +1,14 @@
-function getOSXSigningIdentity () {
-  if (process.platform !== "darwin") {
-    return
-  }
+// function getOSXSigningIdentity () {
+//   if (process.platform !== "darwin") {
+//     return
+//   }
 
-  if (process.env.CLASSROOM_DESKTOP_OSX_DEVELOPER_ID) {
-    return process.env.CLASSROOM_DESKTOP_OSX_DEVELOPER_ID
-  } else {
-    console.log("CLASSROOM_DESKTOP_OSX_DEVELOPER_ID not set, the app will not be signed")
-  }
-}
+//   if (process.env.CLASSROOM_DESKTOP_OSX_DEVELOPER_ID) {
+//     return process.env.CLASSROOM_DESKTOP_OSX_DEVELOPER_ID
+//   } else {
+//     console.log("CLASSROOM_DESKTOP_OSX_DEVELOPER_ID not set, the app will not be signed")
+//   }
+// }
 
 module.exports = {
   // other config here
@@ -26,9 +26,7 @@ module.exports = {
   },
   electronPackagerConfig: {
     packageManager: "npm",
-    osxSign: {
-      identity: getOSXSigningIdentity()
-    },
+    osxSign: true,
     icon: "./app/resources/icon.icns",
     protocols: [
       {
