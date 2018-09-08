9c6f9f05e8043079a55d9908058fe1af273385d4
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2018-09-08 13:17:05 -0500

    code cleanup for mac app signing and revert version

--- forge.config.js
+++ forge.config.js
@@ -1,15 +1,3 @@
-// function getOSXSigningIdentity () {
-//   if (process.platform !== "darwin") {
-//     return
-//   }
-
-//   if (process.env.CLASSROOM_DESKTOP_OSX_DEVELOPER_ID) {
-//     return process.env.CLASSROOM_DESKTOP_OSX_DEVELOPER_ID
-//   } else {
-//     console.log("CLASSROOM_DESKTOP_OSX_DEVELOPER_ID not set, the app will not be signed")
-//   }
-// }
-
 module.exports = {
   // other config here
   make_targets: {
