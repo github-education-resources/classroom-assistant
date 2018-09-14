180f489059fd7207a7586c337ba83965ba5b4489
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2018-09-14 15:11:28 -0500

    add devtools to menu when in dev mode

--- app/menu.js
+++ app/menu.js
@@ -1,7 +1,8 @@
 /* eslint-env node */
 const {app, Menu} = require("electron")
+const isDev = require("electron-is-dev")
 
-const template = [
+const defaultTemplate = [
   {
     label: "Edit",
     submenu: [
@@ -11,7 +12,6 @@ const template = [
       {role: "cut"},
       {role: "copy"},
       {role: "paste"},
-      {role: "pasteandmatchstyle"},
       {role: "delete"},
       {role: "selectall"}
     ]
@@ -28,51 +28,68 @@ const template = [
     submenu: [
       {
         label: "Learn More",
-        click () { require("electron").shell.openExternal("https://www.classroom.github.com/desktop") }
+        click () { require("electron").shell.openExternal("https://www.classroom.github.com/assistant") }
       }
     ]
   }
 ]
 
-export const generateMenu = () => {
-  if (process.platform === "darwin") {
-    template.unshift({
-      label: app.getName(),
-      submenu: [
-        {role: "about"},
-        {type: "separator"},
-        {role: "services", submenu: []},
-        {type: "separator"},
-        {role: "hide"},
-        {role: "hideothers"},
-        {role: "unhide"},
-        {type: "separator"},
-        {role: "quit"}
-      ]
-    })
-
-    // Edit menu
-    template[1].submenu.push(
+const addDarwinOptions = () => {
+  defaultTemplate.unshift({
+    label: app.getName(),
+    submenu: [
+      {role: "about"},
       {type: "separator"},
-      {
-        label: "Speech",
-        submenu: [
-          {role: "startspeaking"},
-          {role: "stopspeaking"}
-        ]
-      }
-    )
-
-    // Window menu
-    template[3].submenu = [
-      {role: "close"},
-      {role: "minimize"},
-      {role: "zoom"},
+      {role: "services", submenu: []},
       {type: "separator"},
-      {role: "front"}
+      {role: "hide"},
+      {role: "hideothers"},
+      {role: "unhide"},
+      {type: "separator"},
+      {role: "quit"}
     ]
+  })
+
+  // Window menu
+  defaultTemplate[3].submenu = [
+    {role: "close"},
+    {role: "minimize"},
+    {role: "zoom"},
+    {type: "separator"},
+    {role: "front"}
+  ]
+}
+
+const addDevOptions = () => {
+  defaultTemplate.unshift({
+    label: "View",
+    submenu: [
+      {
+        label: "Reload",
+        accelerator: "CmdOrCtrl+R",
+        click (item, focusedWindow) {
+          if (focusedWindow) focusedWindow.reload()
+        }
+      },
+      {
+        label: "Toggle Developer Tools",
+        accelerator: process.platform === "darwin" ? "Alt+Command+I" : "Ctrl+Shift+I",
+        click (item, focusedWindow) {
+          if (focusedWindow) focusedWindow.webContents.toggleDevTools()
+        }
+      }
+    ]
+  })
+}
+
+export const generateMenu = () => {
+  if (isDev) {
+    addDevOptions()
+  }
+  if (process.platform === "darwin") {
+    addDarwinOptions()
   }
 
-  const menu = Menu.buildFromTemplate(template)
+  const menu = Menu.buildFromTemplate(defaultTemplate)
   Menu.setApplicationMenu(menu)
 }
