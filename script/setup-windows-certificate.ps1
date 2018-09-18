012aad2097729c97a40c62c0e21c8bd5a5cd6a38
Srinjoy Majumdar <srinjoym@github.com>
2018-09-17 20:40:01 -0700

    update windows cert process

--- script/setup-windows-certificate.ps1
+++ script/setup-windows-certificate.ps1
@@ -1,7 +1,16 @@
 $scriptPath = split-path -parent $MyInvocation.MyCommand.Definition
 
-$env:KEY_LINK | Out-File "$scriptPath\windows-certificate.p7b"
+# Output Base64 encoded Certificate to File
+$env:KEY_LINK | Out-File "$scriptPath\windows-certificate-base64.txt"
 
-$env:ELECTRON_FORGE_ELECTRON_WINSTALLER_CONFIG_CERTIFICATE_FILE = "$scriptPath\windows-certificate.p7b"
+# Decode certificate
+certutil -decode "$scriptPath\windows-certificate-base64.txt" "$scriptPath\windows-certificate.pfx"
 
-echo $env:ELECTRON_FORGE_ELECTRON_WINSTALLER_CONFIG_CERTIFICATE_FILE
+# Remove base64 file
+Remove-Item "$scriptPath\windows-certificate-base64.txt"
+
+# Set Electron Forge Certificate File
+$env:ELECTRON_FORGE_ELECTRON_WINSTALLER_CONFIG_CERTIFICATE_FILE = "$scriptPath\windows-certificate.pfx"
+
+# Remap KEY_PASSWORD to Electron Forge Certificate Password
+$env:ELECTRON_FORGE_ELECTRON_WINSTALLER_CONFIG_CERTIFICATE_PASSWORD = $env:KEY_PASSWORD
