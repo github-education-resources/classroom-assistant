50928b6891ee2892f8fa74f1e91eb711a544fa31
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2018-09-25 00:14:42 -0500

    init attempt

--- app/modules/submissions/selectors.js
+++ app/modules/submissions/selectors.js
@@ -38,3 +38,21 @@ export const progress = createSelector(
     }
   }
 )
+
+export const cloneStatus = createSelector(
+  selected,
+  numSelected,
+  (submissions, numSelected) => {
+    if (progress(submissions, numSelected) === 100) {
+      return "All submissions have been successfully cloned."
+    } else {
+      submissions.forEach(submission => {
+        if (!submission.cloneStatus || submission.cloneStatus === "Cloning Submission...") {
+          // Submissions still cloning
+          return "Download Progress"
+        }
+      })
+      return "All submissions have been processed. Some errors might have occured."
+    }
+  }
+)
--- app/routes/archive/containers/ArchiveProgressPanel.jsx
+++ app/routes/archive/containers/ArchiveProgressPanel.jsx
@@ -2,23 +2,29 @@ import React from "react"
 import PropTypes from "prop-types"
 import { connect } from "react-redux"
 import ProgressBar from "../../shared/components/ProgressBar"
-import {progress} from "../../../modules/submissions/selectors"
+import {progress, cloneStatus} from "../../../modules/submissions/selectors"
 
-const ArchiveProgressPanel = ({progress}) => (
+const ArchiveProgressPanel = ({
+  progress,
+  cloneStatus
+}) => (
   <div className="archive-progress-panel-wrapper">
     <div className="container">
-      <p className="text-center mb-1">Download Progress</p>
-      <ProgressBar cloneProgress={progress}/>
+      <p className="text-center mb-1">{cloneStatus}</p>
+
+      {cloneStatus !== "Download Progress" && progress && <ProgressBar cloneProgress={progress}/>}
     </div>
   </div>
 )
 
 const mapStateToProps = (state) => ({
   progress: progress(state),
+  cloneStatus: cloneStatus(state),
 })
 
 ArchiveProgressPanel.propTypes = {
   progress: PropTypes.number.isRequired,
+  cloneStatus: PropTypes.string,
 }
 
 export default connect(mapStateToProps, null)(ArchiveProgressPanel)
