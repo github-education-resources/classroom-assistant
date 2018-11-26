288f7b94efe28cbdef36bcece0f8fee79e34d1ea
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2018-11-26 00:47:00 -0600

    debugging

--- app/main-process/letsMove.js
+++ app/main-process/letsMove.js
@@ -1,18 +1,23 @@
 import isDev from "electron-is-dev"
 const {app, dialog} = require("electron")
+const log = require("electron-log")
 
 export const moveToApplicationsFolder = () => {
-    if (!app.isInApplicationsFolder()) {
-      const button = dialog.showMessageBox({
-        type: "question",
-        buttons: ["Move to Applications", "Do Not Move"],
-        message: "Move to Applications Folder?",
-        detail: "I can move myself to the Applications folder if you'd like. Please note that the auto-updater will not work on a read-only volume.",
-        defaultId: 1
-      })
+  if (!app.isInApplicationsFolder() && !isDev) {
+    const button = dialog.showMessageBox({
+      type: "question",
+      buttons: ["Move to Applications", "Do Not Move"],
+      message: "Move to Applications Folder?",
+      detail: "I can move myself to the Applications folder if you'd like. Please note that the auto-updater will not work if the app is in a read-only volume.",
+      defaultId: 0
+    })
 
-      if (button == 0) {
+    if (button === 0) {
+      try {
         app.moveToApplicationsFolder()
+      } catch (e) {
+        log.warn(`Failed to move application to Applications Folder: ${e}`)
       }
     }
+  }
 }
