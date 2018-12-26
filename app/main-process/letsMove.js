4931dd3342edd5c10e8e7bc50803d1c498b35e04
Srinjoy Majumdar <srinjoy.majumdar@gmail.com>
2018-12-25 21:29:51 -0800

    maintain state if user does not want warnings

--- app/main-process/letsMove.js
+++ app/main-process/letsMove.js
@@ -1,23 +1,43 @@
 import isDev from "electron-is-dev"
+
 const {app, dialog} = require("electron")
 const log = require("electron-log")
+const Store = require("electron-store")
 
 export const moveToApplicationsFolder = () => {
-  if (!app.isInApplicationsFolder() && !isDev) {
-    const button = dialog.showMessageBox({
+  if (!app.isInApplicationsFolder() && !isDev && shouldRemindUser()) {
+    dialog.showMessageBox({
       type: "question",
       buttons: ["Move to Applications", "Do Not Move"],
       message: "Move to Applications Folder?",
-      detail: "I can move myself to the Applications folder if you'd like. Please note that the auto-updater will not work if the app is in a read-only volume.",
+      detail: "I can move myself to the Applications folder if you'd like. This will ensure that future updates will be installed correctly.",
+      checkboxLabel: "Do not ask me again",
       defaultId: 0
-    })
-
-    if (button === 0) {
-      try {
-        app.moveToApplicationsFolder()
-      } catch (e) {
-        log.warn(`Failed to move application to Applications Folder: ${e}`)
+    }, (response, checkboxChecked) => {
+      if (response === 0) {
+        try {
+          if (!app.moveToApplicationsFolder()) {
+            throw new Error("Failed to move to Applications Folder.")
+          }
+        } catch (e) {
+          log.warn(`Failed to move application to Applications Folder: ${e}`)
+        }
       }
-    }
+
+      if (checkboxChecked) {
+        // Don't remind user again
+        setReminder(false)
+      }
+    })
   }
+}
+
+const shouldRemindUser = () => {
+  const store = new Store()
+  return !store.get("classroom-assistant-move-to-app-folder")
+}
+
+const setReminder = (value) => {
+  const store = new Store()
+  store.set("classroom-assistant-move-to-app-folder", !value)
 }
